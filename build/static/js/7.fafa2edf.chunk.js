6d449ec934133145432c04d98e325e4e30ce75bc
Aritra-Mondal <aritra.mondal.0538@gmail.com>
2021-08-25 19:27:20 +0530

    Quote App

---
+++ build/static/js/7.fafa2edf.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpquotes=this.webpackJsonpquotes||[]).push([[7],{52:function(e,t,s){"use strict";s.r(t);var n=s(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page not found!"})})}}}]);
+//# sourceMappingURL=7.fafa2edf.chunk.js.map
